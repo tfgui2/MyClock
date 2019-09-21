fd74f68c7ed5c04b7c412d831ee60ee4edc0f7bd
tfgui <tfgui2@gmail.com>
2019-09-21 16:22:14 +0900

    a

---
+++ MyClock/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string time = System.DateTime.Now.ToShortTimeString();
+            this.label1.Text = time;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SetDesktopLocation(2200, 900);
+            this.timer1.Start();
+            this.timer1_Tick(this, null);
+        }
+    }
+}
